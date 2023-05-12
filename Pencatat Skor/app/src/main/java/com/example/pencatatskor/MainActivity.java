42426882b72d8b1bd2f9c5df271126a503b8afde
dvlboo <kuhhhckr@gmail.com>
2023-05-13 02:03:27 +0700

    update

--- Pencatat Skor/app/src/main/java/com/example/pencatatskor/MainActivity.java
+++ Pencatat Skor/app/src/main/java/com/example/pencatatskor/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.pencatatskor;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,12 +15,24 @@ public class MainActivity extends AppCompatActivity {
     private int mScoreP1, mScoreP2;
     private TextView mScoreTextP1, mScoreTextP2;
 
+    static final String STATE_SCORE_1 = "Team 1 Score";
+    static final String STATE_SCORE_2 = "Team 2 Score";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mScoreTextP1 = findViewById(R.id.tvTeamSatu);
         mScoreTextP2 = findViewById(R.id.tvTeamDua);
+
+        if (savedInstanceState != null) {
+            mScoreP1 = savedInstanceState.getInt(STATE_SCORE_1);
+            mScoreP2 = savedInstanceState.getInt(STATE_SCORE_2);
+
+            // Set the score text views.
+            mScoreTextP1.setText(String.valueOf(mScoreP1));
+            mScoreTextP2.setText(String.valueOf(mScoreP2));
+        }
     }
 
     public void incrementScore(View view) {
@@ -41,4 +58,47 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        // Change the label of the menu based on the state of the app.
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if(nightMode == AppCompatDelegate.MODE_NIGHT_YES){
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        } else{
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Check if the correct item was clicked.
+        if (item.getItemId() == R.id.night_mode) {
+            // Get the night mode state of the app.
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+            // Set the theme mode for the restarted activity.
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode
+                        (AppCompatDelegate.MODE_NIGHT_NO);
+            } else {
+                AppCompatDelegate.setDefaultNightMode
+                        (AppCompatDelegate.MODE_NIGHT_YES);
+            }
+            // Recreate the activity for the theme change to take effect.
+            recreate();
+        }
+        return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // Save the scores.
+        outState.putInt(STATE_SCORE_1, mScoreP1);
+        outState.putInt(STATE_SCORE_2, mScoreP2);
+        super.onSaveInstanceState(outState);
+
+    }
+
 }
